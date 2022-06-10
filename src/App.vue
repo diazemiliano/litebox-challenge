5212eadbf8d80797ce8dd8658dc0fc8363fdfb1c
Emiliano Díaz <diazemiliano.personal@gmail.com>
2022-06-10 14:38:29 -0300

    Header Styles Done

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,117 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view />
-  </div>
+  <v-app :style="{ ...appStyles }" class="app">
+    <v-app-bar app flat color="transparent" height="72" class="app__header-bar">
+      <div class="app__header-bar__logo d-flex align-start">
+        Lite <span class="app__header-bar__logo--light">Flix</span>
+      </div>
+      <div class="d-flex align-start">
+        <v-btn text class="app__header-bar__text-button" color="white">
+          <v-img
+            :src="require('@/assets/plus-icon.svg')"
+            height="14"
+            width="14"
+          />
+          <span>Agregar Película</span>
+        </v-btn>
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn
+        text
+        color="white"
+        width="27"
+        height="27"
+        min-width="27"
+        max-width="27"
+      >
+        <v-img :src="require('@/assets/app-menu-icon.svg')" />
+      </v-btn>
+      <v-btn
+        text
+        color="white"
+        width="27"
+        height="27"
+        min-width="27"
+        max-width="27"
+      >
+        <v-img :src="require('@/assets/app-bell-icon.svg')" />
+      </v-btn>
+      <v-btn
+        text
+        color="white"
+        width="27"
+        height="27"
+        min-width="27"
+        max-width="27"
+      >
+        <v-avatar size="36px">
+          <img
+            alt="Avatar"
+            src="https://avatars.githubusercontent.com/u/64487650?v=4"
+          />
+        </v-avatar>
+      </v-btn>
+    </v-app-bar>
+
+    <v-main>
+      <router-view />
+    </v-main>
+  </v-app>
 </template>
 
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    appStyles: {
+      background: `url("/Bkg.jpg")`,
+      backgroundSize: `cover`,
+    },
+  }),
+};
+</script>
+
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+.app {
+  &__header-bar {
+    .v-btn {
+      margin-left: 30px;
+    }
 
-nav {
-  padding: 30px;
+    .v-toolbar__content {
+      padding-top: 35px;
+      padding-left: 180px;
+      padding-right: 180px;
+    }
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
+    &__logo {
+      letter-spacing: 4px;
+      font-size: 34px;
+      line-height: 34px;
+      font-weight: 700;
+      color: #64eebc;
+      &--light {
+        font-weight: 300;
+        line-height: 31px;
+      }
+    }
 
-    &.router-link-exact-active {
-      color: #42b983;
+    &__text-button {
+      &.v-btn.v-size--default {
+        font-size: 18px;
+        font-weight: 400;
+        letter-spacing: 4px;
+
+        & .v-image {
+          top: -1px;
+        }
+
+        & span {
+          margin-left: 12px;
+          position: relative;
+        }
+      }
     }
   }
 }
